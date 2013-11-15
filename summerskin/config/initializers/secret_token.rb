6a5a4effc468395f711d59c2ee88ab48d21d133c
brett-anderson <brettanderson16@gmail.com>
2013-11-14 18:20:10 -0600

    first commit

---
+++ summerskin/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Summerskin::Application.config.secret_token = '3fb71106e9b60f475ad16f10da19db8b46b8aab467578be0bd9e01ca36ee435de52831de1e7b4750b02f9d051f0bd0f9282293c3652697cdb32d742dbea100ee'
